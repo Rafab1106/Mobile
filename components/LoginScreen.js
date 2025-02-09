3d511dd4406677b5bd166f3758f11f42ed841b6d
Fabio <153347934+Rafab1106@users.noreply.github.com>
2025-02-09 15:34:55 +0300

    ajouter message erreur

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Alert, TouchableOpacity,Modal } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 import InputField from './InputField';
@@ -11,6 +11,8 @@ const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigation = useNavigation(); // Hook pour la navigation
+  const [error,setError] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
 
   const handleLogin = async () => {
     if (email === '' || password === '') {
@@ -23,7 +25,8 @@ const LoginScreen = () => {
       await AsyncStorage.setItem('userId', userId);
       navigation.navigate('Accueil'); // Navigue vers l'écran d'accueil
     } catch (error) {
-      Alert.alert('Erreur', error.message);
+      setError(error.message);
+      setModalVisible(true);
       console.error('Erreur lors de la connexion:', error.message);
     }
   };
@@ -44,6 +47,24 @@ const LoginScreen = () => {
       <TouchableOpacity onPress={() => navigation.navigate('Register')}>
         {/* <Text style={styles.registerText}>S'inscrire?</Text> */}
       </TouchableOpacity>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}  // Fermer la modal si on appuie en dehors
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>{error}</Text>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.modalButtonText}>Fermer</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -71,6 +92,37 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textDecorationLine: 'underline',
   },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',  // Fond semi-transparent
+  },
+  modalContent: {
+    backgroundColor: '#ffcccc',  // Fond rouge clair pour l'erreur
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    width: 250,
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 20,
+    textAlign: 'center',
+    color: '#d80000',  // Texte rouge pour indiquer l'erreur
+  },
+  modalButton: {
+    backgroundColor: '#d80000',  // Couleur rouge pour le bouton d'erreur
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  modalButtonText: {
+    color: '#fff',  // Texte du bouton en blanc pour contraster avec le rouge
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  
 });
 
 export default LoginScreen;
